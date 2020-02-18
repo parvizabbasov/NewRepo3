fd3fcaa1098d431ec85c5c9f5375ff152074fd16
parvizabbasov <parvizabbasov@hotmail.com>
2020-02-18 14:27:01 +0200

    commit2

--- MyDBProject3/Person/Tables/AddressType.sql
+++ MyDBProject3/Person/Tables/AddressType.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Person].[AddressType] (
     [AddressTypeID] INT              IDENTITY (1, 1) NOT NULL,
-    [Name]          [dbo].[Name]     NOT NULL,
+    [Name]          [dbo].[Name]      NULL,
     [rowguid]       UNIQUEIDENTIFIER CONSTRAINT [DF_AddressType_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
     [ModifiedDate]  DATETIME         CONSTRAINT [DF_AddressType_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_AddressType_AddressTypeID] PRIMARY KEY CLUSTERED ([AddressTypeID] ASC)
